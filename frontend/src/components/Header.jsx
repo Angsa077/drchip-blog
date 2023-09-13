9f6600cf4f5482d079788f90bf90556e4805bce6
Angga Saputra <angsa0077@gmail.com>
2023-09-13 20:54:11 +0700

    menambahkan fitur delete profile picture

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { images } from "../constants";
 import { logout } from '../store/actions/user';
 
 const navItemsInfo = [
-    { name: "Home", type: "link" },
-    { name: "Project", type: "link" },
-    { name: "Pages", type: "dropdown", items: ["About us", "Contact us"] },
-    { name: "Harga", type: "link" },
-    { name: "Ketentuan", type: "link" },
+    { name: "Home", type: "link", href: "/" },
+    { name: "Project", type: "link", href: "/projects" },
+    { name: "Pages", type: "dropdown", items: [{ title: "Tentang Kami", href: "/tentang" }, { title: "Hubungi Kami", href: "/kontak" }] },
+    { name: "Harga", type: "link", href: "/harga" },
+    { name: "Ketentuan", type: "link", href: "/ketentuan" },
 ];
 
 const NavItem = ({ item }) => {
@@ -28,9 +28,9 @@ const NavItem = ({ item }) => {
         <li className='relative group'>
             {item.type === "link" ? (
                 <>
-                    <a href="/" className='px-4 py-2'>
+                    <Link to={item.href} className='px-4 py-2'>
                         {item.name}
-                    </a>
+                    </Link>
                     <span className="cursor-pointer text-dark-light absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
                         /
                     </span>
@@ -44,9 +44,9 @@ const NavItem = ({ item }) => {
                     <div className={`${dropdown ? "block" : 'hidden'} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
                         <ul className="bg-dark-light lg:bg-primary text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
                             {item.items.map((page, index) => (
-                                <a key={index} href="/" className="hover:bg-white lg:hover:bg-[#ffc05b] hover:text-dark-light lg:hover:text-white px-4 py-2 text-white">
-                                    {page}
-                                </a>
+                                <Link key={index} to={page.href} className="hover:bg-white lg:hover:bg-[#ffc05b] hover:text-dark-light lg:hover:text-white px-4 py-2 text-white">
+                                    {page.title}
+                                </Link>
                             ))}
                         </ul>
                     </div>
@@ -76,9 +76,9 @@ const Header = () => {
     return (
         <section className='sticky top-0 left-0 right-0 z-50 bg-white'>
             <header className='container mx-auto px-5 flex justify-between py-4 items-center mb-4'>
-                <div>
+                <Link to="/">
                     <img src={images.Logo} alt="Logo" className="w-40" />
-                </div>
+                </Link>
                 <div className='lg:hidden z-50'>
                     {navIsVisible ? (
                         <AiOutlineClose className='w-6 h-6' onClick={navVisibilityHandler} />
