f164e7882e80e0ae4cab899c01d85f1bc7b010bc
Angga Saputra <angsa0077@gmail.com>
2023-09-13 10:30:38 +0700

    implement redux

--- frontend/src/pages/home/container/Projects.jsx
+++ frontend/src/pages/home/container/Projects.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { FaArrowRight } from 'react-icons/fa'
+import { FaArrowRight } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
 
-import ProjectCard from '../../../components/ProjectCard'
+import ProjectCard from '../../../components/ProjectCard';
+import { changeCount } from '../../../store/actions/countActions';
 
 const Projects = () => {
+    const dispatch = useDispatch();
+    const count = useSelector(state => state.count);
+
+    const countChangeHandler = (type) => {
+        dispatch(changeCount(type))
+    }
+
     return (
         <section className='flex flex-col container mx-auto px-5 py-10 items-center'>
             <div className='flex flex-wrap md:gap-x-5 gap-y-5 justify-center pb-10'>
@@ -15,6 +24,11 @@ const Projects = () => {
                 <span>Lihat lebih banyak</span>
                 <FaArrowRight className='w-3 h-3' />
             </button>
+            <div className='mt-2 flex items-center gap-x-5'>
+                <button onClick={() => countChangeHandler('DECREASE')}>decrease</button>
+                {count.number}
+                <button onClick={() => countChangeHandler('INCREASE')}>increase</button>
+            </div>
         </section>
     );
 };
