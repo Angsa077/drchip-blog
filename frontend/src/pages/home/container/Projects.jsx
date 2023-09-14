26ebacb4bbad6f8bde1da2ac12f14bb2875a2d46
Angga Saputra <angsa0077@gmail.com>
2023-09-14 20:22:11 +0700

    menambahkan loading & error state

--- frontend/src/pages/home/container/Projects.jsx
+++ frontend/src/pages/home/container/Projects.jsx
@@ -5,6 +5,8 @@ import ProjectCard from '../../../components/ProjectCard';
 import { useQuery } from '@tanstack/react-query'
 import { getAllPosts } from '../../../services/index/posts';
 import { toast } from "react-hot-toast";
+import ProjectCardSkeleton from '../../../components/ProjectCardSkeleton';
+import ErrorMessage from '../../../components/ErrorMessage';
 
 const Projects = () => {
     const { data, isLoading, isError } = useQuery({
@@ -19,13 +21,24 @@ const Projects = () => {
     return (
         <section className='flex flex-col container mx-auto px-5 py-10'>
             <div className='flex flex-wrap md:gap-x-5 gap-y-5 justify-center pb-10'>
-                {!isLoading && !isError && data.map((post) => (
-                    <ProjectCard
-                        key={post._id}
-                        post={post}
-                        className="w-full md:w-[calc(50%-20px)] lg:w-[calc(32.5%-21px)]"
-                    />
-                ))}
+                {isLoading ? (
+                    [...Array(3)].map((item, index) => (
+                        <ProjectCardSkeleton
+                            key={index}
+                            className="w-full md:w-[calc(50%-20px)] lg:w-[calc(32.5%-21px)]"
+                        />
+                    ))
+                ) : isError ? (
+                    <ErrorMessage message="Tidak dapat menampilkan halaman" />
+                ) : (
+                    data.map((post) => (
+                        <ProjectCard
+                            key={post._id}
+                            post={post}
+                            className="w-full md:w-[calc(50%-20px)] lg:w-[calc(32.5%-21px)]"
+                        />
+                    ))
+                )}
             </div>
             <button className='mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary hover:bg-primary hover:text-white px-6 py-3 rounded-lg'>
                 <span>Lihat lebih banyak</span>
--- frontend/src/pages/projectDetail/ProjectDetailPage.jsx
+++ frontend/src/pages/projectDetail/ProjectDetailPage.jsx
@@ -16,6 +16,8 @@ import CommentsContainer from '../../components/comments/CommentsContainer';
 import SocialShareButtons from '../../components/SocialShareButtons';
 import { useQuery } from '@tanstack/react-query';
 import { getSinglePost } from '../../services/index/posts';
+import ProjectDetailSkeleton from './components/ProjectDetailSkeleton';
+import ErrorMessage from '../../components/ErrorMessage';
 
 const postsData = [
     {
@@ -64,7 +66,7 @@ const ProjectDetailPage = () => {
     const [breadCrumbsData, setbreadCrumbsData] = useState([]);
     const [body, setBody] = useState(null);
 
-    const { data } = useQuery({
+    const { data, isLoading, isError } = useQuery({
         queryFn: () => getSinglePost({ slug }),
         queryKey: ['project', slug],
         onSuccess: (data) => {
@@ -82,51 +84,55 @@ const ProjectDetailPage = () => {
 
     return (
         <MainLayout>
-            <section className='container mx-auto max-w-7xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start'>
-                <article className='flex-1'>
-                    <BreadCrumbs data={breadCrumbsData} />
-                    <img
-                        className='rounded-xl w-full'
-                        src={data?.photo ? stables.UPLOAD_FOLDER_BASE_URL | data?.photo : images.defaultPostImage}
-                        alt={data?.title}
-                    />
-
-                    <div className='mt-4 flex gap-2'>
-                        {data?.categories.map((kategori) => (
-                            <Link
-                                to={`/project?kategori=${kategori.name}`}
-                                className='text-primary text-sm font-roboto inline-block mt-4 md:text-base'
-                            >
-                                {kategori.name}
-                            </Link>
-                        ))}
-                    </div>
-
-                    <h1 className='text-xl font-medium font-roboto mt-4 text-dark-light md:text-[26px]'>{data?.title}</h1>
-                    <div className='mt-4 prose prose-sm sm:prose-base'>{body}</div>
-                    <CommentsContainer className="mt-10" logginedUserId="a" />
-                </article>
-                <div>
-                    <SuggestedPosts
-                        header="Project Terakhir"
-                        posts={postsData}
-                        tags={tagsData}
-                        className='mt-8 lg:mt-0 lg:max-w-xs'
-                    />
-                    <div className='mt-7'>
-                        <h2 className='font-roboto font-medium text-dark-hard mb-4 md:text-xl'>
-                            Bagikan kepada:
-                        </h2>
-                        <SocialShareButtons
-                            url={encodeURI("https://portfolio-angsa077.vercel.app/"
-                            )}
-                            title={encodeURIComponent(
-                                "My portfolio"
-                            )}
+            {isLoading ? (
+                <ProjectDetailSkeleton />
+            ) : isError ? <ErrorMessage message="Tidak dapat menampilkan halaman" /> : (
+                <section className='container mx-auto max-w-7xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start'>
+                    <article className='flex-1'>
+                        <BreadCrumbs data={breadCrumbsData} />
+                        <img
+                            className='rounded-xl w-full'
+                            src={data?.photo ? stables.UPLOAD_FOLDER_BASE_URL | data?.photo : images.defaultPostImage}
+                            alt={data?.title}
                         />
+
+                        <div className='mt-4 flex gap-2'>
+                            {data?.categories.map((kategori) => (
+                                <Link
+                                    to={`/project?kategori=${kategori.name}`}
+                                    className='text-primary text-sm font-roboto inline-block mt-4 md:text-base'
+                                >
+                                    {kategori.name}
+                                </Link>
+                            ))}
+                        </div>
+
+                        <h1 className='text-xl font-medium font-roboto mt-4 text-dark-light md:text-[26px]'>{data?.title}</h1>
+                        <div className='mt-4 prose prose-sm sm:prose-base'>{body}</div>
+                        <CommentsContainer className="mt-10" logginedUserId="a" />
+                    </article>
+                    <div>
+                        <SuggestedPosts
+                            header="Project Terakhir"
+                            posts={postsData}
+                            tags={tagsData}
+                            className='mt-8 lg:mt-0 lg:max-w-xs'
+                        />
+                        <div className='mt-7'>
+                            <h2 className='font-roboto font-medium text-dark-hard mb-4 md:text-xl'>
+                                Bagikan kepada:
+                            </h2>
+                            <SocialShareButtons
+                                url={encodeURI("https://portfolio-angsa077.vercel.app/"
+                                )}
+                                title={encodeURIComponent(
+                                    "My portfolio"
+                                )}
+                            />
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            )}
         </MainLayout>
     );
 };
